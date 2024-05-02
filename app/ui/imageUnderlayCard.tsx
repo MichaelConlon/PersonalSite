6e722490bcebac845f02e3e06079dd9030e577b2
Michael Conlon <michael@Conlon.io>
2024-05-01 22:11:02 -0400

    fixed mobile styling for imageUnderlayCard Component
    made position of the textbox relative again so that parent div can adjust height accordingly on screen resize

--- app/ui/imageUnderlayCard.tsx
+++ app/ui/imageUnderlayCard.tsx
@@ -2,16 +2,16 @@ import TitledText from "@/app/ui/titledText";
 import backgroundImage from "@/public/work.jpg"
 
 export default function ImageUnderlayCard() {
-        return <div className="relative w-9/12 flex h-96 mb-24">
-                <div style={{ backgroundImage: `url(${backgroundImage.src})`, }} className="absolute h-96 w-7/12 top-0 left-10" >
+        return <div className="relative w-9/12 flex h-96 mt-24 mb-8 flex-row justify-end h-max">
+                <div style={{ backgroundImage: `url(${backgroundImage.src})`, }} className="absolute h-96 w-full lg:w-7/12 -top-24 lg:left-10" >
                 </div>
-                <div className="absolute top-24 py-8 right-0 bg-white w-8/12">
+                <div className="py-8 bg-white w-full lg:w-8/12 z-10">
                     <TitledText 
                         title="Want to work together?"
                         text="Whether you're interested in working together on a single project or offering me a job, I'm always happy to hear from you!" 
                         buttonText="Get in Touch!"
                         buttonLink="/">
-                    </TitledText>
+                    </TitledText> 
                 </div>
         </div>
 }
